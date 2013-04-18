5871d4a5ebf3647f5c14f577e5618322f9876e41
Pascal Essiembre <pascal.essiembre@norconex.com>
2013-04-18 16:06:36 -0400

    Added licensing information and javadoc now part of distribution.

---
+++ norconex-committer-solr/src/main/assembly/README.txt
@@ -0,0 +1,9 @@
+For any inquiries about Norconex Committer Solr, please contact Norconex Inc. at
+info@norconex.com.
+
+This product website and documentation is located at:
+http://www.norconex.com/product/committer-solr/
+
+Source code can be obtained at:
+https://github.com/Norconex/collectors
+
--- norconex-committer-solr/src/main/java/com/norconex/committer/solr/SolrCommitter.java
+++ norconex-committer-solr/src/main/java/com/norconex/committer/solr/SolrCommitter.java
@@ -1,3 +1,21 @@
+/* Copyright 2010-2013 Norconex Inc.
+ * 
+ * This file is part of Norconex Committer Solr.
+ * 
+ * Norconex Committer Solr is free software: you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Norconex Committer Solr is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Norconex Committer Solr. If not, see 
+ * <http://www.gnu.org/licenses/>.
+ */
 package com.norconex.committer.solr;
 
 import java.io.BufferedReader;
@@ -255,6 +273,7 @@ public class SolrCommitter extends BatchableCommitter
             out.println(StringEscapeUtils.escapeXml(
                     line.replaceAll("<.*?>", " ")));
         }
+        in.close();
         
         String contentField = getContentTargetField();
         if (StringUtils.isBlank(contentField)) {
