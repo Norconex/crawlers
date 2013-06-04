97fa035908d5c173e946efe6ba473572403928bf
Pascal Dimassimo <pascal@pascaldimassimo.com>
2013-06-04 15:13:05 -0400

    Added first draft of norconex-committer-elasticsearch

---
+++ norconex-committer-elasticsearch/src/main/java/com/norconex/committer/elasticsearch/IClientFactory.java
@@ -0,0 +1,40 @@
+/* Copyright 2010-2013 Norconex Inc.
+ * 
+ * This file is part of Norconex Committer.
+ * 
+ * Norconex Committer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Norconex Committer is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Norconex Committer. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.norconex.committer.elasticsearch;
+
+import org.elasticsearch.client.Client;
+
+/**
+ * 
+ * Factory for Elasticsearch {@link Client}.
+ * 
+ * @author <a href="mailto:pascal.dimassimo@norconex.com">Pascal Dimassimo</a>
+ * 
+ */
+public interface IClientFactory {
+
+    /**
+     * Creates a client
+     * 
+     * @param committer
+     *            committer object (used to get properties needed for building
+     *            the client)
+     * @return {@link Client}
+     */
+    public Client createClient(ElasticsearchCommitter committer);
+}
