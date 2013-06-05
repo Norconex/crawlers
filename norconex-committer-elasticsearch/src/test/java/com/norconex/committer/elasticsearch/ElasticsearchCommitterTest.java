7e72801f71b997b249ddb9c7205615848fc0e0e7
Pascal Essiembre <pascal.essiembre@norconex.com>
2013-06-05 12:29:37 -0400

    Fixed license info and changed version number.

--- norconex-committer-elasticsearch/src/test/java/com/norconex/committer/elasticsearch/ElasticsearchCommitterTest.java
+++ norconex-committer-elasticsearch/src/test/java/com/norconex/committer/elasticsearch/ElasticsearchCommitterTest.java
@@ -42,6 +42,24 @@ import com.norconex.commons.lang.config.ConfigurationLoader;
 import com.norconex.commons.lang.config.ConfigurationUtil;
 import com.norconex.commons.lang.map.Properties;
 
+/* Copyright 2010-2013 Norconex Inc.
+ * 
+ * This file is part of Norconex ElasticSearch Committer.
+ * 
+ * Norconex ElasticSearch Committer is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation, either version 3 of the License, 
+ * or (at your option) any later version.
+ * 
+ * Norconex ElasticSearch Committer is distributed in the hope that it will be 
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Norconex ElasticSearch Committer. 
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
 public class ElasticsearchCommitterTest {
 
     @Rule
