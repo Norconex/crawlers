7e15c6c0b4e0771528a12eddc581ac95c3b8dcc0
Pascal Essiembre <pascal.essiembre@norconex.com>
2013-09-24 21:48:59 -0400

    removed projects unrelated to HTTP Collector

--- norconex-importer/src/main/scripts/importer.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd %~dp0
-
-java -cp "./lib/*;./classes" com.norconex.importer.Importer %*
-
--- norconex-importer/src/main/scripts/importer.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cd $(dirname $0)
-
-java -cp "./lib/*:./classes" com.norconex.importer.Importer "$@"
