c1f6210fc5437aeabc3d1f06b361a7f246455881
Pascal Essiembre <pascal.essiembre@norconex.com>
2013-10-12 21:34:49 -0400

    New SegmentCountURLFilter class for filtering URLs having a specified number of segments (can check duplicate segments too). New unit tests. Upgraded dependent libraries: Norconex Committer 1.0.1, Norconex Commons Lang 2.0.0, and other thrid party libraries.

---
+++ norconex-collector-http/src/main/java/com/norconex/collector/http/filter/impl/SegmentCountURLFilter.java
@@ -0,0 +1,258 @@
+/* Copyright 2010-2013 Norconex Inc.
+ *
+ * This file is part of Norconex HTTP Collector.
+ *
+ * Norconex HTTP Collector is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Norconex HTTP Collector is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Norconex HTTP Collector. If not,
+ * see <http://www.gnu.org/licenses/>.
+ */
+package com.norconex.collector.http.filter.impl;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+
+import org.apache.commons.configuration.XMLConfiguration;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import com.norconex.collector.http.doc.HttpDocument;
+import com.norconex.collector.http.doc.HttpMetadata;
+import com.norconex.collector.http.filter.IHttpDocumentFilter;
+import com.norconex.collector.http.filter.IHttpHeadersFilter;
+import com.norconex.collector.http.filter.IURLFilter;
+import com.norconex.commons.lang.config.ConfigurationLoader;
+import com.norconex.commons.lang.config.IXMLConfigurable;
+import com.norconex.commons.lang.url.HttpURL;
+import com.norconex.importer.filter.AbstractOnMatchFilter;
+import com.norconex.importer.filter.OnMatch;
+/**
+ * Filters URL based based on the number of URL segments. A URL with
+ * a number of segments equal or more than the specified count will either
+ * be included or excluded, as specified.
+ * <p/>
+ * By default
+ * segments are obtained by breaking the URL text at each forward slashes
+ * (/), starting after the host name.  You can define different or
+ * additional segment separator characters.
+ * <p/>
+ * When <code>duplicate</code> is <code>true</code>, it will count the maximum
+ * number of duplicate segments found.
+ * <p>
+ * XML configuration usage:
+ * </p>
+ * <pre>
+ *  &lt;filter class="com.norconex.collector.http.filter.impl.SegmentCountURLFilter"
+ *          onMatch="[include|exclude]"
+ *          count="(numeric value)"
+ *          duplicate="[false|true]"
+ *          separator="(a regex identifying segment separator)" /&gt;
+ * </pre>
+ * @author Pascal Essiembre
+ * @since 1.2
+ */
+@SuppressWarnings("nls")
+public class SegmentCountURLFilter extends AbstractOnMatchFilter implements
+        IURLFilter,
+        IHttpDocumentFilter,
+        IHttpHeadersFilter,
+        IXMLConfigurable{
+
+    private static final long serialVersionUID = 927290374632128891L;
+
+    public static final String DEFAULT_SEGMENT_SEPARATOR_PATTERN = "/";
+    public static final int DEFAULT_SEGMENT_COUNT = 10;
+
+    private String separator = DEFAULT_SEGMENT_SEPARATOR_PATTERN;
+    private int count = DEFAULT_SEGMENT_COUNT;
+    private boolean duplicate;
+    private Pattern separatorPattern;
+
+    public SegmentCountURLFilter() {
+        this(DEFAULT_SEGMENT_COUNT);
+    }
+    public SegmentCountURLFilter(int count) {
+        this(count, OnMatch.INCLUDE);
+    }
+    public SegmentCountURLFilter(
+            int count, OnMatch onMatch) {
+        this(count, onMatch, false);
+    }
+    public SegmentCountURLFilter(
+            int count, OnMatch onMatch, boolean duplicate) {
+        super();
+        setCount(count);
+        setOnMatch(onMatch);
+        setDuplicate(duplicate);
+        setSeparator(DEFAULT_SEGMENT_SEPARATOR_PATTERN);
+    }
+
+    /**
+     * Gets the segment separator pattern
+     * @return the pattern
+     */
+    public String getSeparator() {
+        return separator;
+    }
+    public final void setSeparator(String separator) {
+        this.separator = separator;
+        if (StringUtils.isNotBlank(separator)) {
+            this.separatorPattern = Pattern.compile(separator);
+        } else {
+            this.separatorPattern = Pattern.compile(
+                    DEFAULT_SEGMENT_SEPARATOR_PATTERN);
+        }
+    }
+
+    public int getCount() {
+        return count;
+    }
+    public final void setCount(int count) {
+        this.count = count;
+    }
+
+    public boolean isDuplicate() {
+        return duplicate;
+    }
+    public final void setDuplicate(boolean duplicate) {
+        this.duplicate = duplicate;
+    }
+
+    @Override
+    public boolean acceptDocument(HttpDocument document) {
+        return acceptURL(document.getUrl());
+    }
+    @Override
+    public boolean acceptDocument(String url, HttpMetadata headers) {
+        return acceptURL(url);
+    }
+    @Override
+    public boolean acceptURL(String url) {
+        boolean isInclude = getOnMatch() == OnMatch.INCLUDE;
+        if (StringUtils.isBlank(separator)) {
+            return isInclude;
+        }
+        String path = new HttpURL(url).getPath();
+        String[] allSegments = separatorPattern.split(path);
+        // remove empty/nulls
+        List<String> cleanSegments = new ArrayList<String>();
+        for (String segment : allSegments) {
+            if (StringUtils.isNotBlank(segment)) {
+                cleanSegments.add(segment);
+            }
+        }
+
+        boolean reachedCount = false;
+        if (duplicate) {
+            Map<String, Integer> segMap = new HashMap<String, Integer>();
+            for (String seg : cleanSegments) {
+                Integer dupCount = segMap.get(seg);
+                if (dupCount == null) {
+                    dupCount = 0;
+                }
+                dupCount++;
+                if (dupCount >= count) {
+                    reachedCount = true;
+                    break;
+                }
+                segMap.put(seg, dupCount);
+            }
+        } else {
+            reachedCount = cleanSegments.size() >= count;
+        }
+
+        return reachedCount && isInclude || !reachedCount && !isInclude;
+    }
+
+    @Override
+    public void loadFromXML(Reader in) {
+        XMLConfiguration xml = ConfigurationLoader.loadXML(in);
+        setSeparator(xml.getString(""));
+        super.loadFromXML(xml);
+        setCount(xml.getInt("[@count]", DEFAULT_SEGMENT_COUNT));
+        setDuplicate(xml.getBoolean("[@duplicate]", false));
+        setSeparator(xml.getString(
+                "[@separator]", DEFAULT_SEGMENT_SEPARATOR_PATTERN));
+    }
+    @Override
+    public void saveToXML(Writer out) throws IOException {
+        XMLOutputFactory factory = XMLOutputFactory.newInstance();
+        try {
+            XMLStreamWriter writer = factory.createXMLStreamWriter(out);
+            writer.writeStartElement("filter");
+            writer.writeAttribute("class", getClass().getCanonicalName());
+            super.saveToXML(writer);
+            writer.writeAttribute("count", Integer.toString(count));
+            writer.writeAttribute("duplicate", Boolean.toString(duplicate));
+            writer.writeAttribute("separator", separator);
+            writer.writeEndElement();
+            writer.flush();
+            writer.close();
+        } catch (XMLStreamException e) {
+            throw new IOException("Cannot save as XML.", e);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.DEFAULT_STYLE)
+            .appendSuper(super.toString())
+            .append("count", count)
+            .append("separator", separator)
+            .append("duplicate", duplicate)
+            .toString();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder()
+            .appendSuper(super.hashCode())
+            .append(count)
+            .append(separator)
+            .append(duplicate)
+            .toHashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof SegmentCountURLFilter)) {
+            return false;
+        }
+        SegmentCountURLFilter other = (SegmentCountURLFilter) obj;
+        return new EqualsBuilder()
+            .appendSuper(super.equals(obj))
+            .append(count, other.count)
+            .append(separator, other.separator)
+            .append(duplicate, other.duplicate)
+            .isEquals();
+    }
+}
+
---
+++ norconex-collector-http/src/test/java/com/norconex/collector/http/filter/impl/SegmentCountURLFilterTest.java
@@ -0,0 +1,86 @@
+/* Copyright 2010-2013 Norconex Inc.
+ *
+ * This file is part of Norconex HTTP Collector.
+ *
+ * Norconex HTTP Collector is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Norconex HTTP Collector is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Norconex HTTP Collector. If not,
+ * see <http://www.gnu.org/licenses/>.
+ */
+package com.norconex.collector.http.filter.impl;
+
+import java.io.IOException;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.norconex.commons.lang.config.ConfigurationUtil;
+import com.norconex.importer.filter.OnMatch;
+
+public class SegmentCountURLFilterTest {
+
+    SegmentCountURLFilter f;
+    String url;
+
+    @After
+    public void tearDown() throws Exception {
+        f = null;
+        url = null;
+    }
+
+
+    @Test
+    public void testSegmentMatches() throws IOException {
+        //--- Test proper segment counts ---
+        url = "http://www.example.com/one/two/three/four/five/page.html";
+        f = new SegmentCountURLFilter(5, OnMatch.EXCLUDE);
+        Assert.assertFalse("URL wrongfully rejected.", f.acceptURL(url));
+        f = new SegmentCountURLFilter(6, OnMatch.EXCLUDE);
+        Assert.assertFalse("URL wrongfully accepted.", f.acceptURL(url));
+        f = new SegmentCountURLFilter(7, OnMatch.EXCLUDE);
+        Assert.assertTrue("URL wrongfully rejected.", f.acceptURL(url));
+
+        //--- Test proper duplicate counts ---
+        url = "http://www.example.com/aa/bb/aa/cc/bb/aa/aa/bb/cc/dd.html";
+        f = new SegmentCountURLFilter(3, OnMatch.EXCLUDE, true);
+        Assert.assertFalse("URL wrongfully rejected.", f.acceptURL(url));
+        f = new SegmentCountURLFilter(4, OnMatch.EXCLUDE, true);
+        Assert.assertFalse("URL wrongfully accepted.", f.acceptURL(url));
+        f = new SegmentCountURLFilter(5, OnMatch.EXCLUDE, true);
+        Assert.assertTrue("URL wrongfully rejected.", f.acceptURL(url));
+
+        //--- Test custom separator (query string ---
+        url = "http://www.example.com/one/two_three|four-five/page.html";
+        f = new SegmentCountURLFilter(5, OnMatch.EXCLUDE);
+        f.setSeparator("[/_|-]");
+        Assert.assertFalse("URL wrongfully rejected.", f.acceptURL(url));
+        f = new SegmentCountURLFilter(6, OnMatch.EXCLUDE);
+        f.setSeparator("[/_|-]");
+        Assert.assertFalse("URL wrongfully accepted.", f.acceptURL(url));
+        f = new SegmentCountURLFilter(7, OnMatch.EXCLUDE);
+        f.setSeparator("[/_|-]");
+        Assert.assertTrue("URL wrongfully rejected.", f.acceptURL(url));
+    }
+
+    @Test
+    public void testWriteRead() throws IOException {
+        SegmentCountURLFilter f = new SegmentCountURLFilter();
+        f.setCount(5);
+        f.setDuplicate(true);
+        f.setOnMatch(OnMatch.EXCLUDE);
+        f.setSeparator("[/&]");
+        System.out.println("Writing/Reading this: " + f);
+        ConfigurationUtil.assertWriteRead(f);
+    }
+
+}
