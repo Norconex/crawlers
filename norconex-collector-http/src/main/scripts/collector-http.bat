63f30a81ee0d6b63a971e6a148c9c9c257873e4c
Pascal Essiembre <pascal.essiembre@norconex.com>
2014-12-05 23:44:00 -0500

    Updated site upload path and improved launch scripts.

--- norconex-collector-http/src/main/scripts/collector-http.bat
+++ norconex-collector-http/src/main/scripts/collector-http.bat
@@ -1,5 +1,5 @@
 @echo off
 cd %~dp0
+set ROOT_DIR=%~dp0
 
-java -Dfile.encoding=UTF8 -cp "./lib/*;./classes" com.norconex.collector.http.HttpCollector %*
-
+java -Dlog4j.configuration="file:///%ROOT_DIR%classes/log4j.properties" -Dfile.encoding=UTF8 -cp "./lib/*;./classes" com.norconex.collector.http.HttpCollector %*
