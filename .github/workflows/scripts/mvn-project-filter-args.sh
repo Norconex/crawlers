569133d37baac7dcff6369fd41764531b7805d44
Pascal Essiembre <pascal.essiembre@norconex.com>
2023-05-12 01:25:09 -0400

    Work on Mono repo workflow.

--- .github/workflows/scripts/mvn-project-filter-args.sh
+++ .github/workflows/scripts/mvn-project-filter-args.sh
@@ -8,4 +8,42 @@
 # Relies on  ./github/outputs to be present, previously generated
 # by 'tj-actions/changed-files' action.
 
-echo $(ls -l .github/outputs) 
+
+OUTPUTS_DIR=.github/outputs
+CHANGES_FILE=all_changed_files.txt
+
+parent_changed=$(cd $OUTPUTS_DIR; cat $CHANGES_FILE | grep -v -e ^committer -e ^importer -e ^crawler | wc -l)
+
+if [ $parent_changed -gt 999999 ]; then
+    # maven parent has changed, so don't filter to build all projects
+    echo "";
+    exit;
+fi
+
+# Filter on modified artifacts
+
+#artifacts=$(cat $changed_dirs | grep -e ^committer -e ^importer -e ^crawler | sed 's_^\(committer\|crawler\)/\([^\s]*\)_:\1-\2_' | sed 's_^\(importer\)/.*$_:\1_' | sort | uniq)
+
+#dirs=($(cat $changed_dirs))
+
+#echo "DIRS: ${dirs[1]}"
+
+
+
+
+#artifacts=$(echo $dirs | grep -e ^committer -e ^importer -e ^crawler | sed 's_^\(committer\|crawler\)/\([^/]*\)/.*$_:\1-\2_' | sed 's_^\(importer\)/.*$_:\1_' | sort | uniq)
+#artifacts=$(cat $changed_dirs | grep -e ^committer -e ^importer -e ^crawler | sed 's_^\(committer\|crawler\)/\([^/]*\)/.*$_:\1-\2_' | sed 's_^\(importer\)/.*$_:\1_' | sort | uniq)
+artifacts=$(cd $OUTPUTS_DIR; cat $CHANGES_FILE | tr ' ' '\n' | grep -e ^committer -e ^importer -e ^crawler | sed 's_^\(committer\|crawler\)/\(.*\)$_:\1-\2_' | sed 's_^\(importer\)/.*$_:\1_' | sort | uniq | tr '\n' ' ')
+
+echo "ARTIFACTS: $artifacts"
+
+#If this returns, do a regular maven build on parent:
+#cat deleteme.txt | grep -v -e committer -e importer -e crawler | wc -l
+
+#Else, maven build only affected ones
+
+#cat deleteme.txt | grep -e ^committer -e ^importer -e ^crawler | sed 's_^\(committer\|crawler\)/\([^/]*\)/.*$_:\1-\2_' | sed 's_^\(importer\)/.*$_:\1_' | sort | uniq
+
+
+
+#echo $(ls -l .github/outputs) 
