05019a3c4b06e7597c859af5c935336bce0ed79b
Utsav Vanodiya <utsav.vanodiya@norconex.com>
2023-09-07 15:55:38 -0400

    Temporarily commenting XMLAdapters class related lines as it is not available in commons-lang jar.

--- crawler/fs/src/main/java/com/norconex/crawler/fs/fetch/impl/package-info.java
+++ crawler/fs/src/main/java/com/norconex/crawler/fs/fetch/impl/package-info.java
@@ -1,15 +1,15 @@
 /**
  * File System Fetcher implementations.
  */
-@XmlJavaTypeAdapters({
-    @XmlJavaTypeAdapter(
-            value=XMLAdapters.DurationAdapter.class, type=Duration.class)
-})
+//@XmlJavaTypeAdapters({
+//    @XmlJavaTypeAdapter(
+//            value=XMLAdapters.DurationAdapter.class, type=Duration.class)
+//})
 package com.norconex.crawler.fs.fetch.impl;
 
 import java.time.Duration;
 
-import com.norconex.commons.lang.xml.XMLAdapters;
+//import com.norconex.commons.lang.xml.XMLAdapters;
 
 import jakarta.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 import jakarta.xml.bind.annotation.adapters.XmlJavaTypeAdapters;
