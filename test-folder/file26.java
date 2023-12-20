889a71d32c9438f537e647f688390b4aaf2e7f43
Brian Yuen <98049573+brian-yuen@users.noreply.github.com>
2023-12-20 10:14:45 -0700

    Create file26.java

---
+++ test-folder/file26.java
@@ -0,0 +1,13 @@
+/*
+ * 
+ * 
+ * Copyright 2018 Norconex Inc.
+ * 
+ * 
+ */
+
+public class file26_1 {
+    main afdasf dsfsfgasdsfsdsafasdfafdssfsadasdafsdsadfasfssfgfsesfsdfchan
+    changed-files
+    sfsafdfsasfasdffdfsdfssafsdfsfsdsgf
+}
