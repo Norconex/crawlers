1a260e85768af2d1f00fc4220b6b8976904b68f2
Brian Yuen <98049573+brian-yuen@users.noreply.github.com>
2023-12-20 11:45:25 -0700

    Create file36.java

---
+++ test-folder/file36.java
@@ -0,0 +1,13 @@
+/*
+ * 
+ * 
+ * Copyright 2008-2011 Norconex Inc.
+ * 
+ * 
+ */
+
+public class file36_1 {
+    main afdasf dsfsfgasdsfsdsafasdfafdssfsadasdafsdsadfasfssfgfsesfsdfchan
+    changed-files
+    sfsafdfsasfasdffdfsdfssafsdfsfsdsgf
+}
