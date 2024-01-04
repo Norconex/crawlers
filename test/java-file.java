9c0f1dec54af6b4de3c061b8b806b134554b8fc0
Brian Yuen <98049573+brian-yuen@users.noreply.github.com>
2024-01-04 09:08:00 -0700

    Create java-file.java

---
+++ test/java-file.java
@@ -0,0 +1,14 @@
+/*
+ * 
+ * 
+ * Copyright 2009 Norconex Inc.
+ * 
+ * 
+ */
+
+public class file1 {
+
+    main afdasf dsfsfgasdsfsdsafasdfafdssadasdafsdsadfasfssfgfsesfsdfchan
+    changed-files
+    sfsafdfsasfasdffdfsdfssafsdfsfsdsgfadasd
+  }
