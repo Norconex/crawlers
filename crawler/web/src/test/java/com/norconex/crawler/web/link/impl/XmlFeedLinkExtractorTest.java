6b44a582f067c5aff32fdc32e42e2cc6cdd8646d
Harinder Hanjan <harinder.hanjan@gmail.com>
2024-01-22 09:24:04 -0700

    Dummy Update XmlFeedLinkExtractorTest.java

--- crawler/web/src/test/java/com/norconex/crawler/web/link/impl/XmlFeedLinkExtractorTest.java
+++ crawler/web/src/test/java/com/norconex/crawler/web/link/impl/XmlFeedLinkExtractorTest.java
@@ -12,6 +12,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+// Delete me
+
 package com.norconex.crawler.web.link.impl;
 
 import static org.assertj.core.api.Assertions.assertThatNoException;
