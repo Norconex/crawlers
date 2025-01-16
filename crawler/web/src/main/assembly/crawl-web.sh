0630ed335ca4d3127331d7cd5fc33d18a3199582
Harinder Hanjan <harinder.hanjan@gmail.com>
2025-01-16 13:21:41 -0700

    Add ignite options to run scripts

--- crawler/web/src/main/assembly/crawl-web.sh
+++ crawler/web/src/main/assembly/crawl-web.sh
@@ -24,4 +24,33 @@ export ROOT_DIR=$(realpath $(dirname $0))
 #     -DenableJMX=true
 
 
-java -Dlog4j2.configurationFile="file:${ROOT_DIR}/log4j2.xml" -Dfile.encoding=UTF8 -cp "./lib/*" com.norconex.crawler.web.WebCrawler "$@"
+java -Dlog4j2.configurationFile="file:${ROOT_DIR}/log4j2.xml" \
+-Dfile.encoding=UTF8 \
+-Xms512m \
+-Xmx512m \
+-Djava.net.preferIPv4Stack=true \
+--add-opens=java.base/jdk.internal.access=ALL-UNNAMED \
+--add-opens=java.base/jdk.internal.misc=ALL-UNNAMED \
+--add-opens=java.base/sun.nio.ch=ALL-UNNAMED \
+--add-opens=java.base/sun.util.calendar=ALL-UNNAMED \
+--add-opens=java.management/com.sun.jmx.mbeanserver=ALL-UNNAMED \
+--add-opens=jdk.internal.jvmstat/sun.jvmstat.monitor=ALL-UNNAMED \
+--add-opens=java.base/sun.reflect.generics.reflectiveObjects=ALL-UNNAMED \
+--add-opens=jdk.management/com.sun.management.internal=ALL-UNNAMED \
+--add-opens=java.base/java.io=ALL-UNNAMED \
+--add-opens=java.base/java.nio=ALL-UNNAMED \
+--add-opens=java.base/java.net=ALL-UNNAMED \
+--add-opens=java.base/java.util=ALL-UNNAMED \
+--add-opens=java.base/java.util.concurrent=ALL-UNNAMED \
+--add-opens=java.base/java.util.concurrent.locks=ALL-UNNAMED \
+--add-opens=java.base/java.util.concurrent.atomic=ALL-UNNAMED \
+--add-opens=java.base/java.lang=ALL-UNNAMED \
+--add-opens=java.base/java.lang.invoke=ALL-UNNAMED \
+--add-opens=java.base/java.math=ALL-UNNAMED \
+--add-opens=java.sql/java.sql=ALL-UNNAMED \
+--add-opens=java.base/java.lang.reflect=ALL-UNNAMED \
+--add-opens=java.base/java.time=ALL-UNNAMED \
+--add-opens=java.base/java.text=ALL-UNNAMED \
+--add-opens=java.management/sun.management=ALL-UNNAMED \
+--add-opens java.desktop/java.awt.font=ALL-UNNAMED \
+-cp "./lib/*" com.norconex.crawler.web.WebCrawler "$@"
