b16a816eba5cad97c62addca726d3316389c9258
Harinder Hanjan <harinder.hanjan@gmail.com>
2025-02-06 12:01:44 -0700

    minor bug fix

--- crawler/web/src/main/assembly/crawl-web.sh
+++ crawler/web/src/main/assembly/crawl-web.sh
@@ -52,5 +52,5 @@ java -Dlog4j2.configurationFile="file:${ROOT_DIR}/log4j2.xml" \
 --add-opens=java.base/java.time=ALL-UNNAMED \
 --add-opens=java.base/java.text=ALL-UNNAMED \
 --add-opens=java.management/sun.management=ALL-UNNAMED \
---add-opens java.desktop/java.awt.font=ALL-UNNAMED \
+--add-opens=java.desktop/java.awt.font=ALL-UNNAMED \
 -cp "./lib/*" com.norconex.crawler.web.WebCrawler "$@"
